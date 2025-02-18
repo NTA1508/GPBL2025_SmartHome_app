2dc7e61b4bd8418fe9c929d63bfc28c7595ef4b1
whalelab12 <jobugorira211@gmail.com>
2025-02-18 17:00:40 +0900

    update 1

--- app/src/main/java/com/example/smarthome/MainActivity.kt
+++ app/src/main/java/com/example/smarthome/MainActivity.kt
@@ -3,120 +3,75 @@ package com.example.smarthome
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.Switch
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.*
 
-class automation : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
-    var database = FirebaseDatabase.getInstance()
-    var myRef = database.reference
-    internal lateinit var btnSwitch: Switch
-    internal lateinit var btnSwitch1:Switch
-    internal lateinit var btnSwitch2:Switch
+    private val database = FirebaseDatabase.getInstance()
+    private val myRef = database.reference
+    private lateinit var btnSwitch: Switch
+    private lateinit var btnSwitch1: Switch
+    private lateinit var btnSwitch2: Switch
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnSwitch = findViewById(R.id.switch2)
+        btnSwitch1 = findViewById(R.id.toggleButton4)
+        btnSwitch2 = findViewById(R.id.toggleButton5)
+
         myRef.child("Toggle/switch").addValueEventListener(object : ValueEventListener {
-            override fun onCancelled(p0: DatabaseError) {
+            override fun onCancelled(error: DatabaseError) {}
 
-            }
-            override fun onDataChange(p0: DataSnapshot) {
-                switch1.visibility = View.VISIBLE
-                if (p0.value.toString().equals("1")){
-                    switch1.isChecked = true;
-                    toggleButton2.visibility = View.VISIBLE
-                    toggleButton3.visibility = View.VISIBLE
+            override fun onDataChange(snapshot: DataSnapshot) {
+                btnSwitch.visibility = View.VISIBLE
+                if (snapshot.value.toString() == "1") {
+                    btnSwitch.isChecked = true
+                    btnSwitch1.visibility = View.VISIBLE
+                    btnSwitch2.visibility = View.VISIBLE
 
-                    myRef.child("automation/AC").addValueEventListener(object :ValueEventListener{
-                        override fun onCancelled(p0: DatabaseError) {
+                    myRef.child("automation/AC").addValueEventListener(object : ValueEventListener {
+                        override fun onCancelled(error: DatabaseError) {}
 
-                        }
-
-                        override fun onDataChange(p0: DataSnapshot) {
-                            if(p0.value.toString().equals("1")){
-                                toggleButton2.isChecked = true
-                            }
-                            else
-                                toggleButton2.isChecked = false
-
+                        override fun onDataChange(snapshot: DataSnapshot) {
+                            btnSwitch1.isChecked = snapshot.value.toString() == "1"
                         }
                     })
 
-                    myRef.child("automation/light").addValueEventListener(object :ValueEventListener{
-                        override fun onCancelled(p0: DatabaseError) {
+                    myRef.child("automation/light").addValueEventListener(object : ValueEventListener {
+                        override fun onCancelled(error: DatabaseError) {}
 
-                        }
-
-                        override fun onDataChange(p0: DataSnapshot) {
-                            if(p0.value.toString().equals("1")){
-                                toggleButton3.isChecked = true
-                            }
-                            else
-                                toggleButton3.isChecked = false
-
+                        override fun onDataChange(snapshot: DataSnapshot) {
+                            btnSwitch2.isChecked = snapshot.value.toString() == "1"
                         }
                     })
+                } else {
+                    btnSwitch.isChecked = false
+                    btnSwitch1.visibility = View.GONE
+                    btnSwitch2.visibility = View.GONE
                 }
-                else{
-                    switch1.isChecked = false
-                    toggleButton2.visibility = View.GONE
-                    toggleButton3.visibility = View.GONE
-                }
-
-                // Log.d("ahsan", p0.value.toString())
             }
         })
 
-        button3.setOnClickListener{
+        findViewById<Button>(R.id.button1).setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
 
-        btnSwitch = findViewById<View>(R.id.switch1) as Switch
-        btnSwitch1 = findViewById<View>(R.id.toggleButton2) as Switch
-        btnSwitch2 = findViewById<View>(R.id.toggleButton3) as Switch
+        btnSwitch.setOnClickListener {
+            myRef.child("Toggle/switch").setValue(if (btnSwitch.isChecked) "1" else "0")
+        }
 
-        btnSwitch.setOnClickListener{
-            if(btnSwitch.isChecked)
-            {
-                myRef.child("Toggle/switch").setValue("1")
+        btnSwitch1.setOnClickListener {
+            myRef.child("automation/AC").setValue(if (btnSwitch1.isChecked) "1" else "0")
+        }
 
-                btnSwitch1.setOnClickListener{
-                    if(btnSwitch1.isChecked)
-                    {
-                        myRef.child("automation/AC").setValue("1")
-                    }
-                    else
-                    {
-                        myRef.child("automation/AC").setValue("0")
-                    }
-                }
-
-
-                btnSwitch2.setOnClickListener{
-                    if(btnSwitch2.isChecked)
-                    {
-                        myRef.child("automation/light").setValue("1")
-                    }
-                    else
-                    {
-                        myRef.child("automation/light").setValue("0")
-                    }
-                }
-            }
-            else
-            {
-                myRef.child("Toggle/switch").setValue("0")
-            }
+        btnSwitch2.setOnClickListener {
+            myRef.child("automation/light").setValue(if (btnSwitch2.isChecked) "1" else "0")
         }
     }
 }
